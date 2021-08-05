ee2d41ea22e4eaa2913ababdcec419e584e8ef41
lion160801 <82790079+lion160801@users.noreply.github.com>
2021-08-05 18:05:58 +0700

    [Main] exerciseQuestion

--- toeicOnline/toeic-core-service/src/main/java/vn/myclass/core/service/ExerciseQuestionService.java
+++ toeicOnline/toeic-core-service/src/main/java/vn/myclass/core/service/ExerciseQuestionService.java
@@ -1,4 +1,8 @@
 package vn.myclass.core.service;
 
+import java.util.Map;
+
 public interface ExerciseQuestionService {
+    Object[] findExerciseQuestionByProperties(Map<String,Object> property, String sortExpression, String sortDirection, Integer offset, Integer limit);
+
 }
--- toeicOnline/toeic-web-logic/src/main/java/vn/myclass/controller/web/ExerciseController.java
+++ toeicOnline/toeic-web-logic/src/main/java/vn/myclass/controller/web/ExerciseController.java
@@ -3,7 +3,6 @@ package vn.myclass.controller.web;
 import org.apache.commons.lang.StringUtils;
 import vn.myclass.command.ExerciseCommand;
 import vn.myclass.core.dto.ExerciseDTO;
-import vn.myclass.core.dto.ListenGuidelineDTO;
 import vn.myclass.core.web.common.WebConstant;
 import vn.myclass.core.web.utils.FormUtil;
 import vn.myclass.core.web.utils.RequestUtil;
@@ -20,43 +19,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet(urlPatterns = {"/danh-sach-bai-tap-nghe.html"})
+@WebServlet(urlPatterns = {"/danh-sach-bai-tap.html"})
 public class ExerciseController extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ExerciseCommand command = FormUtil.populate(ExerciseCommand.class,request);
-        ExerciseDTO pojo = command.getPojo();
-        if(pojo.getExerciseId()!=null){
-
-        }else {
-            excuteSearchExercise(request,command);
-            request.setAttribute(WebConstant.LIST_ITEMS,command);
-            RequestDispatcher rd = request.getRequestDispatcher("/views/web/exercise/list.jsp");
-            rd.forward(request,response);
-        }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ExerciseCommand command = FormUtil.populate(ExerciseCommand.class, request);
+        executeSearchExercise(request, command);
+        request.setAttribute(WebConstant.LIST_ITEMS, command);
+        RequestDispatcher rd = request.getRequestDispatcher("/views/web/exercise/list.jsp");
+        rd.forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-
-    }
-    private void excuteSearchExercise(HttpServletRequest request, ExerciseCommand command) {
-        Map<String, Object> properties = buildeMapProperties(command);
+    private void executeSearchExercise(HttpServletRequest request, ExerciseCommand command) {
+        Map<String, Object> properties = buildMapProperties(command);
         command.setMaxPageItems(3);
         RequestUtil.initSearchBeanManual(command);
-        Object[] objects = SingletonServiceUtil.getExerciseServiceInstance().findExerciseByProperties(properties, command.getSortExpression(),
-                command.getSortDirection(),command.getFirstItem(),command.getMaxPageItems());
+        Object[] objects = SingletonServiceUtil.getExerciseServiceInstance().findExerciseByProperties(properties, command.getSortExpression(), command.getSortDirection(), command.getFirstItem(), command.getMaxPageItems());
         command.setListResult((List<ExerciseDTO>) objects[1]);
         command.setTotalItems(Integer.parseInt(objects[0].toString()));
-        command.setTotalPages((int)Math.ceil((double) command.getTotalItems() / command.getMaxPageItems()));
+        command.setTotalPages((int) Math.ceil((double) command.getTotalItems() / command.getMaxPageItems()));
     }
 
-    private Map<String, Object> buildeMapProperties(ExerciseCommand command) {
+    private Map<String,Object> buildMapProperties(ExerciseCommand command) {
         Map<String, Object> properties = new HashMap<String, Object>();
         if (StringUtils.isNotBlank(command.getPojo().getName())) {
             properties.put("name", command.getPojo().getName());
         }
-        if(command.getPojo().getType()!=null){
+        if (command.getPojo().getType() != null) {
             properties.put("type", command.getPojo().getType());
         }
         return properties;
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     }
 }
