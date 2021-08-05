645a131d2b7b67cd33134a5949b0a871981ac610
lion160801 <82790079+lion160801@users.noreply.github.com>
2021-08-05 14:56:23 +0700

    [Main] show list listen-exercise

--- toeicOnline/toeic-core-service-impl/src/main/java/vn/myclass/core/service/impl/ExerciseServiceImpl.java
+++ toeicOnline/toeic-core-service-impl/src/main/java/vn/myclass/core/service/impl/ExerciseServiceImpl.java
@@ -1,6 +1,28 @@
 package vn.myclass.core.service.impl;
 
+import vn.myclass.core.dto.ExerciseDTO;
+import vn.myclass.core.dto.ListenGuidelineDTO;
+import vn.myclass.core.persistence.entity.ExerciseEntity;
+import vn.myclass.core.persistence.entity.ListenGuidelineEntity;
 import vn.myclass.core.service.ExerciseService;
+import vn.myclass.core.service.utils.SingletonDaoUtil;
+import vn.myclass.core.utils.ExerciseBeanUtil;
+import vn.myclass.core.utils.ListenGuidelineBeanUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ExerciseServiceImpl implements ExerciseService {
+    @Override
+    public Object[] findExerciseByProperties(Map<String, Object> property, String sortExpression, String sortDirection, Integer offset, Integer limit) {
+        List<ExerciseDTO> result = new ArrayList<ExerciseDTO>();
+        Object[] objects = SingletonDaoUtil.getExerciseDaoInstance().findByProperty(property,sortExpression,sortDirection,offset,limit);
+        for (ExerciseEntity item: (List<ExerciseEntity>)objects[1]){
+            ExerciseDTO dto = ExerciseBeanUtil.entity2Dto(item);
+            result.add(dto);
+        }
+        objects[1] = result;
+        return objects;
+    }
 }
--- toeicOnline/toeic-web-logic/src/main/java/vn/myclass/command/ExerciseCommand.java
+++ toeicOnline/toeic-web-logic/src/main/java/vn/myclass/command/ExerciseCommand.java
@@ -4,5 +4,15 @@ import vn.myclass.core.dto.ExerciseDTO;
 import vn.myclass.core.web.command.AbstractCommand;
 
 public class ExerciseCommand extends AbstractCommand<ExerciseDTO> {
-    public ExerciseCommand(){this.pojo = new ExerciseDTO();};
+    public ExerciseCommand(){this.pojo = new ExerciseDTO();}
+
+    private String exerciseType ;
+
+    public String getExerciseType() {
+        return exerciseType;
+    }
+
+    public void setExerciseType(String exerciseType) {
+        this.exerciseType = exerciseType;
+    }
 }
